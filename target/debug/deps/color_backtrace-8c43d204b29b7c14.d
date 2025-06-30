44d34c6eea28c753371e43cb9cac241134f19aad
shmulisamry <shmulikeller@gmail.com>
2025-06-29 21:45:36 -0400

    added gitignore

--- target/debug/deps/color_backtrace-8c43d204b29b7c14.d
+++
@@ -1,7 +0,0 @@
-/Users/shmuli/repositories/rust-compiler-2/target/debug/deps/libcolor_backtrace-8c43d204b29b7c14.rmeta: /Users/shmuli/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/color-backtrace-0.7.0/src/lib.rs
-
-/Users/shmuli/repositories/rust-compiler-2/target/debug/deps/libcolor_backtrace-8c43d204b29b7c14.rlib: /Users/shmuli/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/color-backtrace-0.7.0/src/lib.rs
-
-/Users/shmuli/repositories/rust-compiler-2/target/debug/deps/color_backtrace-8c43d204b29b7c14.d: /Users/shmuli/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/color-backtrace-0.7.0/src/lib.rs
-
-/Users/shmuli/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/color-backtrace-0.7.0/src/lib.rs:
--- target/debug/deps/rustc_demangle-0c64f19ff7682909.d
+++
@@ -1,9 +0,0 @@
-/Users/shmuli/repositories/rust-compiler-2/target/debug/deps/librustc_demangle-0c64f19ff7682909.rmeta: /Users/shmuli/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rustc-demangle-0.1.25/src/lib.rs /Users/shmuli/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rustc-demangle-0.1.25/src/legacy.rs /Users/shmuli/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rustc-demangle-0.1.25/src/v0.rs
-
-/Users/shmuli/repositories/rust-compiler-2/target/debug/deps/librustc_demangle-0c64f19ff7682909.rlib: /Users/shmuli/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rustc-demangle-0.1.25/src/lib.rs /Users/shmuli/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rustc-demangle-0.1.25/src/legacy.rs /Users/shmuli/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rustc-demangle-0.1.25/src/v0.rs
-
-/Users/shmuli/repositories/rust-compiler-2/target/debug/deps/rustc_demangle-0c64f19ff7682909.d: /Users/shmuli/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rustc-demangle-0.1.25/src/lib.rs /Users/shmuli/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rustc-demangle-0.1.25/src/legacy.rs /Users/shmuli/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rustc-demangle-0.1.25/src/v0.rs
-
-/Users/shmuli/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rustc-demangle-0.1.25/src/lib.rs:
-/Users/shmuli/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rustc-demangle-0.1.25/src/legacy.rs:
-/Users/shmuli/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rustc-demangle-0.1.25/src/v0.rs:
